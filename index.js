8aedfb7d53edc284736d9843a6f386c1ff04aa81
lokisho <oscar.sanchez@gorillalogic.com>
2021-02-04 13:35:51 -0600

    feat(slide_3): associative examples

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const compose = (f, g) => {
   };
 };
 
-const head = (x) => {
-  return x[0];
-};
+const head = (x) => x[0];
 
 const reverse = (x) => x.reduce( (acc, i) => {
   return [i].concat(acc);
@@ -17,6 +15,7 @@ const reverse = (x) => x.reduce( (acc, i) => {
 
 const toUpperCase = (x) => x.toUpperCase();
 
+const exclaim = (x)  => x + '!';
 // Composition is associative, meaning it doesn't matter how you group two of them.
 // The associative property is a math rule that says that the way in which factors are grouped
 // in a multiplication/addition problem does not change the product.
@@ -34,6 +33,25 @@ console.log('compose_2', lastUpper_2(['jumpkick', 'roundhouse', 'uppercut']));
 //  we can give compose as many fn's as we like and let it decide how to group them.
 const compose_variadic = (...fns) => (...args) => fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
 
-lastUpper = compose_variadic(toUpperCase, head, reverse);
+const lastUpper = compose_variadic(toUpperCase, head, reverse);
 console.log('compose_variadic', lastUpper(['jumpkick', 'roundhouse', 'uppercut']));
 // UPPERCUT
+
+// One pleasant benefit of associativity is that any group of functions can be extracted and bundled together
+// in their very own composition.  Here we start playing with our lego.
+let loudLastUpper = compose_variadic(exclaim, toUpperCase, head, reverse);
+console.log('lego_1', loudLastUpper(['jumpkick', 'roundhouse', 'uppercut']));
+// UPPERCUT!
+
+// or
+let last = compose(head, reverse);
+loudLastUpper = compose_variadic(exclaim, toUpperCase, last);
+console.log('lego_2', loudLastUpper(['jumpkick', 'roundhouse', 'uppercut']));
+// UPPERCUT!
+
+// or
+last = compose(head, reverse);
+let angry = compose(exclaim, toUpperCase);
+loudLastUpper = compose(angry, last);
+console.log('lego_2', loudLastUpper(['jumpkick', 'roundhouse', 'uppercut']));
+// UPPERCUT!
