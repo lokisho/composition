3d1b87c6b46f74443ae64e620f2c1d895d4eabc4
lokisho <oscar.sanchez@gorillalogic.com>
2021-02-04 21:18:41 -0600

    feat(slides): slide_6

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-// Slide_5
-// PointFree / Currying.
+// Slide_6
+// Debugging.
 
-const { compose, join, map, pipe, replace, split } = require('ramda');
+const { compose, join, map,  replace, split, tap, toLower } = require('ramda');
 
-const toLowerCase = (x) => x.toLowerCase();
-const toUpperCase = (x) => x.toUpperCase();
-const head = (x) => x[0];
-
-// Pointfree means functions that never mention the data upon which they operate
-//not pointfree because we mention the data: word
-const snakeCase = function(word) {
-  return word.toLowerCase().replace(/\s+/ig, '_');
+const trace = (tag) => (x) => {
+    console.log(tag, x);
+    return x;
 };
+// const trace = curry(function(tag, x) {
+//   console.log(tag, x);
+//   return x;
+// });
+let desherize;
+dasherize = compose(join('-'), toLower, split(' '), replace(/\s{2,}/ig, ' '));
 
-// pointfree + currying
-const pointfreeSnakeCase = compose(replace(/\s+/ig, '_'), toLowerCase);
+// uncomment to trace.
+// dasherize = compose(join('-'), toLower, trace('after split'), split(' '), replace(/\s{2,}/ig, ' '));
+// after split [ 'The', 'world', 'is', 'a', 'vampire' ]
+// dasherize = compose(join('-'), toLower, tap(console.log), split(' '), replace(/\s{2,}/ig, ' '));
+// dasherize = compose(join('-'), toLower, tap((data) => console.log('data', data)), split(' '), replace(/\s{2,}/ig, ' '));
+// [ 'The', 'world', 'is', 'a', 'vampire' ]
 
-console.log('point', snakeCase('to Snake Case'));
-console.log('pointFree', pointfreeSnakeCase('to Snake Case'));
-
-// we don't need the data to construct our function in the pointfree version,
-// whereas in the pointful one, we must have our `word` available before anything else.
-// this help us remove needless names and keep us concise and generic
-
-//not pointfree because we mention the data: name
-const initials = (name) => name.split(' ').map(pipe(head, toUpperCase)).join('. ');
-console.log('initials', initials("hunter stockton thompson"));
-// 'H. S. T'
-
-//pointfree
-const pointfreeInitials = pipe(
-    split(' '),
-    map(pipe(head, toUpperCase)),
-    join('. ')
-);
-
-console.log('initialsPointfree', pointfreeInitials("hunter stockton thompson"));
-// 'H. S. T'
+console.log(dasherize('The world is a vampire'));
