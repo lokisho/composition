45f7f3a74b1899fe7651892d44b681b14754c79c
lokisho <oscar.sanchez@gorillalogic.com>
2021-02-08 13:19:28 -0600

    feat(slides): slide_8

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-// Slide_4
-// Utility Libraries.
-
-const _ = require('lodash');
-const R = require('ramda');
-
-const head = (x) => x[0];
-
-const reverse = (x) => x.reduce( (acc, i) => [i].concat(acc), []);
-
-const toUpperCase = (x) => x.toUpperCase();
-
-const exclaim = (x)  => x + '!';
-
-// For lodash we have flow & flowRight.
-
-const flowRight = _.flowRight(exclaim, toUpperCase, head, reverse);
-console.log('flowRight', flowRight(['jumpkick', 'roundhouse', 'uppercut']));
-// UPPERCUT!
-
-const flow = _.flow(reverse, head, toUpperCase, exclaim);
-console.log('flow', flow(['jumpkick', 'roundhouse', 'uppercut']));
-// UPPERCUT!
-
-// For ramda we have compose & pipe
-
-const compose = R.compose(exclaim, toUpperCase, head, reverse);
-//console.log('compose', compose(['jumpkick', 'roundhouse', 'uppercut']));
-// UPPERCUT!
-
-const pipe = R.pipe(reverse, head, toUpperCase, exclaim);
-// console.log('pipe', pipe(['jumpkick', 'roundhouse', 'uppercut']));
-// UPPERCUT!
+"use strict";
+// Slide_8
+// Typescript.
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const R = __importStar(require("ramda"));
+var typeOfPet;
+(function (typeOfPet) {
+    typeOfPet["dog"] = "dog";
+    typeOfPet["snake"] = "snake";
+    typeOfPet["fish"] = "fish";
+})(typeOfPet || (typeOfPet = {}));
+const alert = (dog) => console.log(dog.bark);
+const getPet = (typeOfPet) => {
+    var _a;
+    const dog = {
+        bark: 'guau',
+        breed: 'zaguatito',
+    };
+    const snake = {
+        poison: true,
+    };
+    const fish = {
+        river: true
+    };
+    const petsAvailable = {
+        dog,
+        snake,
+        fish,
+    };
+    return (_a = petsAvailable[typeOfPet]) !== null && _a !== void 0 ? _a : petsAvailable.fish;
+};
+const compose = R.pipe(getPet, alert);
+compose('fish');
+// const getFish = ():Fish => ({river: true});
+// alert(getFish());
---
+++ index.ts
@@ -0,0 +1,49 @@
+// Slide_8
+// Typescript.
+
+import * as R from 'ramda';
+
+interface Dog {
+  bark: string;
+  breed: string;
+}
+
+interface Snake {
+  poison: boolean;
+}
+
+interface Fish {
+  river: boolean
+}
+
+enum typeOfPet {
+  dog = 'dog',
+  snake = 'snake',
+  fish = 'fish',
+}
+
+const alert = (dog: Dog) => console.log(dog.bark);
+const getPet = (typeOfPet: string) : Dog | Snake | Fish => {
+  const dog: Dog = {
+    bark: 'guau',
+    breed: 'zaguatito',
+  }
+  const snake: Snake = {
+    poison: true,
+  }
+  const fish: Fish = {
+    river: true
+  }
+  const petsAvailable = {
+    dog,
+    snake,
+    fish,
+  }
+  return petsAvailable[typeOfPet] ?? petsAvailable.fish;
+}
+
+const getFish = ():Fish => ({river: true});
+alert(getPet('dog'));
+
+// const compose = R.pipe(getPet, alert);
+// compose('fish');
